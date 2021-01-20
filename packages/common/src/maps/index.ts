37966c43bb88750f70e3c6e5469c2cce4faa59bc
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2021-01-20 14:04:07 +0100

    Remove useless maps

--- packages/common/src/maps/index.ts
+++ packages/common/src/maps/index.ts
@@ -1,13 +1,5 @@
 import { DungeonArgs, RoomTemplate } from '@halftheopposite/dungeon';
-import { TMX } from '../tiled';
-import gigantic from './gigantic.json';
 import roomsJSON from './rooms.json';
-import small from './small.json';
-
-export const List: { [key: string]: TMX.IMap } = {
-    small,
-    gigantic,
-};
 
 const rooms: RoomTemplate[] = roomsJSON as RoomTemplate[];
 export const DEFAULT_DUNGEON: DungeonArgs = {

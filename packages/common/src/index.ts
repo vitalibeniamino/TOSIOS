b41d648d5d5170de45ff1930b37bca9739e8e284
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2021-01-24 16:22:53 +0100

    Remove unused module

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,6 +1,5 @@
 import * as Collisions from './collisions';
 import * as Constants from './constants';
-import * as Entities from './entities';
 import * as Geometry from './geometry';
 import * as Keys from './keys';
 import * as Map from './map';
@@ -10,4 +9,4 @@ import * as Models from './models';
 import * as Sorts from './sort';
 import * as Types from './types';
 
-export { Collisions, Constants, Entities, Geometry, Keys, Maths, Map, Maps, Models, Sorts, Types };
+export { Collisions, Constants, Geometry, Keys, Maths, Map, Maps, Models, Sorts, Types };

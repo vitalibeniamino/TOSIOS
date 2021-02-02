66daeb2f02b751c9f39555b89d92624e6c46b540
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2021-02-02 12:37:33 +0100

    Add missing impact manager commit from previous one

--- packages/client/src/game/managers/ImpactManager.ts
+++ packages/client/src/game/managers/ImpactManager.ts
@@ -1,5 +1,11 @@
-import { Bullet, Prop } from '../entities';
-import { BulletImpactConfig, ImpactTexture, Trail25Texture, WoodExplosionConfig } from '../assets/particles';
+import { Bullet, Monster, Prop } from '../entities';
+import {
+    BulletImpactConfig,
+    ImpactTexture,
+    MonsterImpactConfig,
+    PropImpactConfig,
+    Trail25Texture,
+} from '../assets/particles';
 import { Container, Texture } from 'pixi.js';
 import { Emitter, OldEmitterConfig } from 'pixi-particles';
 
@@ -17,7 +23,11 @@ export class ImpactsManager extends Container {
     }
 
     spawnPropImpact(prop: Prop) {
-        this.spawnImpact(prop.x, prop.y, WoodExplosionConfig as OldEmitterConfig, [Trail25Texture]);
+        this.spawnImpact(prop.x, prop.y, PropImpactConfig as OldEmitterConfig, [Trail25Texture]);
+    }
+
+    spawnMonsterImpact(monster: Monster) {
+        this.spawnImpact(monster.x, monster.y, MonsterImpactConfig as OldEmitterConfig, [Trail25Texture]);
     }
 
     private spawnImpact(x: number, y: number, config: OldEmitterConfig, textures: Texture[]) {
